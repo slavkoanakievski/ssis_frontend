c166545ab9494e9fc91d369d27ae38a6667b647a
Slavko Anakievski <anakievskislavko@gmail.com>
2022-09-04 16:58:48 +0200

    added server backend v2

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://ssislearning.azurewebsites.net",
+    baseURL: "https://ssislearning.azurewebsites.net/api/",
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
